b8bd780e6ed1c581c6d06507c10498737273a46a
MAriefW <wicaksonoma15d@student.unhas.ac.id>
2021-03-27 23:08:31 +0800

    ganti tulisan Teknik -> Tehnik

--- views/index.php
+++ views/index.php
@@ -27,7 +27,7 @@ session_start();
 
     <nav class="navbar navbar-expand-lg navbar-blue shadow">
         <div class="container">
-            <a class="navbar-brand font-weight-bolder" href="#">PT Anugrah Teknik Mandiri</a>
+            <a class="navbar-brand font-weight-bolder" href="#">PT Anugrah Tehnik Mandiri</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
