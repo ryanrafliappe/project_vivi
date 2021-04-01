ab660fe261968405bda522faa1a4dd5be6e02bbb
MAriefW <wicaksonoma15d@student.unhas.ac.id>
2021-04-01 18:47:14 +0800

    Tambahan panduan di halaman utama

--- views/Home/index.php
+++ views/Home/index.php
@@ -1,7 +1,8 @@
 <div class="container mt-5" style="background-image: url('../assets/img/intro-bg.jpg');background-repeat: no-repeat;background-size: cover; background-position: center;">
     <div class="row justify-content-center">
-
+<h4 style="color:red">Panduan pemesanan produk dapat dilihat pada file <a href="panduan.pdf">ini</a></h3>
         <div class="col-lg-6 mt-5 ">
+
             <h1 class="display-4 text-center">Selamat Datang!</h1>
 
         </div>
